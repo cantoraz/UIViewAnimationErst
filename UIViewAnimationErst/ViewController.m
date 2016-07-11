651fe4d08da343ee74a293f42d5c60ad76258540
Cantoraz Chou <cantoraz@gmail.com>
2016-07-11 13:30:22 +0800

    ADDED: Add spring animation to login button.

--- UIViewAnimationErst/ViewController.m
+++ UIViewAnimationErst/ViewController.m
@@ -61,13 +61,23 @@
                      }
                      completion:NULL];
     
+    // Animate the loginButton
+    /*
     CGAffineTransform rotation = CGAffineTransformMakeRotation((CGFloat)M_PI);
-//    CGAffineTransform scale = CGAffineTransformMakeScale(2.f, 2.f);
+//    CGAffineTransform scale = CGAffineTransformMakeScale(2., 2.);
 //    CGAffineTransform rotationAndScale = CGAffineTransformConcat(rotation, scale);
-    [UIView animateWithDuration:1.f animations:^{
+    [UIView animateWithDuration:1. animations:^{
         _loginButton.transform = rotation;
 //        _loginButton.transform = rotationAndScale;
+        
     }];
+     */
+
+    [UIView animateWithDuration:3. delay:.5 usingSpringWithDamping:0.5 initialSpringVelocity:0. options:UIViewAnimationOptionAllowUserInteraction animations:^{
+        CGPoint loginButtonCenterPoint = CGPointMake(_loginButton.center.x, _loginButton.center.y - 30.);
+        _loginButton.center = loginButtonCenterPoint;
+        _loginButton.alpha = 1.;
+    } completion:NULL];
     
     [super viewDidAppear:animated];
 }
@@ -76,6 +86,22 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Recieved Actions
+
+// Touch up inside loginButton
+- (IBAction)login:(id)sender {
+    [UIView animateWithDuration:.5 delay:0. usingSpringWithDamping:.2 initialSpringVelocity:0. options:UIViewAnimationOptionAllowUserInteraction animations:^{
+        CGRect bounds = CGRectMake(_loginButton.bounds.origin.x, _loginButton.bounds.origin.y,
+                                   _loginButton.bounds.size.width + 25, _loginButton.bounds.size.height);
+        _loginButton.bounds = bounds;
+    } completion:NULL];
+    
+    [UIView animateWithDuration:.6 delay:0. usingSpringWithDamping:.6 initialSpringVelocity:0. options:UIViewAnimationOptionAllowUserInteraction animations:^{
+        CGPoint center = CGPointMake(_loginButton.center.x, _loginButton.center.y + 10);
+        _loginButton.center = center;
+    } completion:NULL];
 }
 
 @end
